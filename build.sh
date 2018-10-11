d5ca9df9f3587a9a9efcdf13a3950024b8e22cb9
Greg Cockburn <gergnz@gmail.com>
2018-10-12 07:54:54 +1100

    fix build

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 find ./ -maxdepth 1 \! -name ".git" \! -name "." -type d -printf "%P\\n" | \
 while read -r dir
 do
-  docker build -t "$DOCKER_USERNAME/$dir:latest"
+  docker build -t "$DOCKER_USERNAME/$dir:latest" "$dir"
   docker push "$DOCKER_USERNAME/$dir:latest"
 done
 

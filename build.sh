6016aefe1517f80d425e97690b5c16b56813bb61
Greg Cockburn <gergnz@gmail.com>
2018-10-14 13:23:59 +1100

    make it easy to see what's being build

--- build.sh
+++ build.sh
@@ -8,6 +8,9 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 find ./ -maxdepth 1 \! -name ".git" \! -name "." -type d -printf "%P\\n" | \
 while read -r dir
 do
+  echo "#############################################################"
+  echo "Building $dir"
+  echo "#############################################################"
   docker build -t "$DOCKER_USERNAME/$dir:latest" "$dir"
   docker push "$DOCKER_USERNAME/$dir:latest"
 done

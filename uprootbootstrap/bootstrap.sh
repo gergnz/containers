c4a3579574614eb966280a52b041638bc4064bf3
Greg Cockburn <gergnz@gmail.com>
2018-10-16 08:12:55 +1100

    add ssh key manipulation

--- uprootbootstrap/bootstrap.sh
+++ uprootbootstrap/bootstrap.sh
@@ -1,5 +1,7 @@
 #! /bin/bash -e
 
+region=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/.$//')
+
 mkdir -p /efs/shared/jenkins
 chown 1000:1000 /efs/shared/jenkins
 
@@ -11,5 +13,20 @@ touch /efs/shared/pritunl/etc/pritunl.conf
 
 mkdir -p /efs/shared/build-kite
 echo 'tags="queue=docker"' > /efs/shared/build-kite/buildkite-agent.cfg
+
+mkdir -p /efs/shared/build-kite/buildkite-secrets/pxadmin/urbis_pro_support_infra
+aws --region "$region" ssm get-parameter --name /pxadmin/urbis_pro_support_infra/access-key --output text --query 'Parameter.Value' --with-decryption | base64 -D > /efs/shared/build-kite/buildkite-secrets/pxadmin/urbis_pro_support_infra/access-key
+
+mkdir -p /efs/shared/build-kite/hooks
+cat > /efs/shared/build-kite/hooks/environment
+#!/bin/bash
+
+set -euo pipefail
+
+eval "$(ssh-agent -s)"
+ssh-add -k /buildkite-secrets/pxadmin/urbis_pro_support_infra
+EOF
+
+chmod +x /efs/shared/build-kite/hooks/environment
 
 while true; do sleep 10; done

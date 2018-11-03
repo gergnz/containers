abcef16bc70eeacd06990923b3b9fdd7d4d73abd
Greg Cockburn <gergnz@gmail.com>
2018-11-04 09:36:17 +1100

    add codedeploy

---
+++ codedeploy/codedeployagent.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+/opt/codedeploy-agent/bin/codedeploy-agent start
+
+while sleep 60; do
+  ps -o pid,cmd | grep "codedeploy-agent: master" | grep -q -v grep
+  ret_val=$?
+  if [[ $ret_val -ne 0 ]]
+  then
+    exit 1
+  fi
+done

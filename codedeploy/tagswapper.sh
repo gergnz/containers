b991a83d6546d78c7f535717d2e342aba7a48e6b
Greg Cockburn <gergnz@gmail.com>
2018-11-04 21:46:39 +1100

    remove hard coded param

--- codedeploy/tagswapper.sh
+++ codedeploy/tagswapper.sh
@@ -3,7 +3,7 @@
 instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 region=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/.$//')
 
-asg_cluster=$(aws --region "$region" autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].Tags[?Value==`amstaging-ecs`].ResourceId' --output text)
+asg_cluster=$(aws --region "$region" ec2 describe-instances --instance-ids "$instance_id" --query 'Reservations[0].Instances[0].Tags[?Key==`aws:autoscaling:groupName`].Value' --output text)
 
 instance_ids=$(aws --region "$region" autoscaling describe-auto-scaling-groups --auto-scaling-group-names "$asg_cluster" --query 'AutoScalingGroups[0].Instances[*].InstanceId' --output text)
 

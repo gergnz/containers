3d6f0de2202187437f897a08781da1ffee8294c3
Greg Cockburn <gergnz@gmail.com>
2019-01-04 17:16:39 +1100

    add cloudflared

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ set -e
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
+wget https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-linux-amd64.tgz
+tar -C ./cloudflared -zxf cloudflared-stable-linux-amd64.tgz
+
 find ./ -maxdepth 1 \! -name ".git" \! -name "." -type d -printf "%P\\n" | \
 while read -r dir
 do

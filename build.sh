15491b06b06da49073ff87993da4d2c436bf3ef5
Greg Cockburn <gergnz@gmail.com>
2019-01-14 10:28:45 +1100

    remove container building and make cloudflared self-contained

--- build.sh
+++ build.sh
@@ -5,19 +5,6 @@ set -e
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-wget https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-linux-amd64.tgz
-tar -C ./cloudflared -zxf cloudflared-stable-linux-amd64.tgz
-
-find ./ -maxdepth 1 \! -name ".git" \! -name "." -type d -printf "%P\\n" | \
-while read -r dir
-do
-  echo "#############################################################"
-  echo "Building $dir"
-  echo "#############################################################"
-  docker build -t "$DOCKER_USERNAME/$dir:latest" "$dir"
-  docker push "$DOCKER_USERNAME/$dir:latest"
-done
-
 echo "#############################################################"
 echo "Building caddy"
 echo "#############################################################"

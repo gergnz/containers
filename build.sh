1ddac29a74a80823048000412847748dfacfb591
Greg Cockburn <gergnz@gmail.com>
2019-01-14 12:15:59 +1100

    add caddy

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 echo "#############################################################"
 echo "Building caddy"
 echo "#############################################################"
-docker build -t "$DOCKER_USERNAME/caddy:latest" --build-arg     plugins=route53     github.com/abiosoft/caddy-docker.git
-docker push "$DOCKER_USERNAME/caddy:latest"
+#docker build -t "$DOCKER_USERNAME/caddy:latest" --build-arg     plugins=route53     github.com/abiosoft/caddy-docker.git
+#docker push "$DOCKER_USERNAME/caddy:latest"
